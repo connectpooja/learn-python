c86b160ce97ff953192b0723ee288e8ee8c6899e
Pooja <connectb.pooja@gmail.com>
2024-08-21 01:09:24 +0530

    ft. examples of variable

--- variables.py
+++ variables.py
@@ -26,3 +26,33 @@ print(floatVariable)
 print(type(stringVariable))
 print(type(integerVariable))
 print(type(floatVariable))
+
+#More on Variables
+#Usage of Single Quotes and Double Quotes
+x = "Pooja"
+#this is the exact same thing as writing 
+x = 'Pooja'
+
+#variable name are case sensitive
+a = 5
+A = 7
+
+pooja = 7
+Pooja = 8
+PoOja = 9
+PoOjA = 10
+
+#my variable -->  #this is not acceptable because there is a space in between it
+#my-variable --> Because there is a hyphen in the middle, but the actual reason is python is trating the hyphen as substraction
+
+#below is some examples of acceptable variable naming conventions
+myVariable = "Fine"
+myvariable = "This is also fine"
+my_variable = "This is also fine"
+_myVariable = "This is also fine"
+MYVAR = "This is also fine"
+
+myVar2 = "This is acceptable"
+#but
+# 2myVar -> This is not acceptable
+
